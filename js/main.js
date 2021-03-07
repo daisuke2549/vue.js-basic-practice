d1c5f1d47063dcbb9972bf39d9c5b3a7d3b2d8c9
daisuke sasaki <gerund25495641@gmail.com>
2021-03-07 17:02:48 +0900

    条件付きレンダリングについて学習済み
    v-if = 要素をdomから削除、追加
    v-else,v-else-ifが使用できる。
    
    v-show
    v-else, v-else-ifが使えない。
    css display プロパティ

--- js/main.js
+++ js/main.js
@@ -9,14 +9,68 @@
 
 
 /* 算出プロパティーについて */
+// var app = new Vue({
+//     el:'#app',
+//     data:{
+//         message: "hello world"
+//     },
+//     computed:{
+//         reversedMessage:  function(){
+//             return this.message.split('').reverse().join('')
+//         }
+//     },
+//     methods:{
+//         reversedMessageMethod : function(){
+//             return this.message.split('').reverse().join('')
+//         }
+//     }
+// })
+
+
+/* 算出プロパティ-とメソッドの比較について */
+
+
+// var app = new Vue({
+//     el:'#app',
+//     data:{
+//         basePrice :100
+//     },
+//     computed:{
+//         taxIncludedPrice:{
+//              get: function(){
+//                  return parseInt(this.basePrice *1.08)
+//              },
+//              set:function(taxIncludedPrice){
+//                  this.basePrice = Math.ceil(taxIncludedPrice / 1.08)
+//              }
+//         }
+//     }
+// })
+
+
+
+// var app = new Vue({
+//     el: "#app",
+//     computed:{
+//         computedNumber: function(){
+//             return Math.random()
+//         }
+//     },
+//     methods:{
+//         methodsNumber: function(){
+//             return Math.random()
+//         }
+//     }
+// })
+
+
+
+/* 条件付きレンダリング */
+
 var app = new Vue({
     el:'#app',
-    data:{
-        message: "hello world"
-    },
-    computed:{
-        reversedMessage:  function(){
-            return this.message.split('').reverse().join('')
-        }
-    }
+    data: {
+     toggle:true
+}
 })
+
